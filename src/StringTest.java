1dca38559b827cfa42f9bf4814cc167adffde988
Otani Jun <ojune16@gmail.com>
2016-09-26 20:35:38 +0900

    add Method shiftOperationTest

--- src/StringTest.java
+++ src/StringTest.java
@@ -1,10 +1,12 @@
+import static org.hamcrest.CoreMatchers.is;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 public class StringTest {
 
-
 	@Test
-	public void TestConstractor() {
+	public void constractorTest() {
 		String name = "Jun Otani";
 		System.out.println(name); // Output: Jun Otani
 
@@ -13,5 +15,24 @@ public class StringTest {
 		int codePoints[] = { codePointOfJ, codePointOft };
 		String stringOfcodePoints = new String(codePoints, 0, 2);
 		System.out.println(stringOfcodePoints);// Output: Ja
+		
+		String a = new String(new int[] { 100, 110012 }, 0, 2);
+		System.out.println(a);
+		System.out.println(a.length());
+
+	}
+
+	@Test
+	public void shiftOperationTest() {
+
+		// 右シフト演算符号あり
+		Assert.assertThat(-100 >> 1, is(-50));
+
+		// 右シフト演算符号なし(補数を考慮しない)
+		Assert.assertThat(-100 >>> 1, is(2147483598));
+
+		// 左シフト演算符号あり
+		Assert.assertThat(-100 << 1, is(-200));
+
 	}
 }
